40c5df20b83ede24ad430c670600defaeee5382b
Utsav0777 <utsavgupta073@gmail.com>
2026-09-10 13:57:06 +0530

    v1.1

--- hms.py
+++ hms.py
@@ -1,5 +1,12 @@
 def admit_patient(patient_id, ward):
     print("Patient", patient_id, "admitted to", ward)
 
+
 def discharge_patient(patient_id):
     print("Patient", patient_id, "discharged")
+
+
+def calculate_bill(days, rate=1000):
+    bill = days * rate
+    print("Bill = Rs.", bill)
+    return bill
